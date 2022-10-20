d32fa18d7e81c4b497ac592ec3e7ebee90d8c002
Z3r0PC <z3r0pc@pop-os.localdomain>
2022-10-19 22:24:15 -0300

    Added functionality to choose number of linked cards

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 let cardCount = prompt('Com quantas cartas você deseja jogar? Selecione uma quantidade par de 4 à 14 cartas.')
+
+while (cardCount < 4 || cardCount > 14 || cardCount % 2 !== 0) {
+    cardCount = prompt('Com quantas cartas você deseja jogar? Selecione uma quantidade par de 4 à 14 cartas.')
+}
+
+{/* <div class="closed-card-container ">
+    <img src="./assets/back.png" alt="">
+</div> */}
